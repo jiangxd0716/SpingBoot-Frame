653e5dba8a81f4ec07195dd1217b615c36639203
jiangxd <806089486@qq.com>
2021-04-25 12:14:12 +0800

    xss过滤器

--- src/main/java/com/tem/frame/FrameApplication.java
+++ src/main/java/com/tem/frame/FrameApplication.java
@@ -4,10 +4,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+/**
+ * SpringBoot 启动类
+ *
+ * @author jiangxd
+ */
 @Slf4j
 @SpringBootApplication(scanBasePackages = "com.tem")
-@MapperScan(basePackages = "com.tem.frame.dao")
+@MapperScan(basePackages = "com.tem.frame.*.dao")
+@ServletComponentScan
 public class FrameApplication {
 
     public static void main(String[] args) {
